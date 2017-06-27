64d60810ead89bea8078f7a61f4628b0755a6d29
Greg Westneat <greg@fosterwebmarketing.com>
2017-06-27 12:04:12 -0700

    updated port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
