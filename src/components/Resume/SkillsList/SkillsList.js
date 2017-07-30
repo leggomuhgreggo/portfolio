b602e99a102b580c8e5d6ae25f3f7762f896edfd
Greg Westneat <greg@fosterwebmarketing.com>
2017-07-30 14:26:18 -0700

    added travis

--- src/components/Resume/SkillsList/SkillsList.js
+++ src/components/Resume/SkillsList/SkillsList.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Tag = styled.span`
-	background: #f0f0f0;
-	display: inline-block;
-	padding: .3em .8em;
-	font-size: .65em;
-	border-radius: 3px;
-	font-family: sans-serif;
+  background: #f0f0f0;
+  display: inline-block;
+  padding: .3em .8em;
+  font-size: .65em;
+  border-radius: 3px;
+  font-family: sans-serif;
   margin: 1px 2px;
-	color: #444;
+  color: #444;
 `;
 
 const TagGroup = props =>
   <div className="skill-type-wrap">
-    <label>{props.label}: </label>
+    <label>
+      {props.label}:{" "}
+    </label>
     <div className="tag-group">
       {props.tags.map((tag, key) =>
-        <Tag key={key} color={tag.color}>{tag}</Tag>
+        <Tag key={key} color={tag.color}>
+          {tag}
+        </Tag>
       )}
     </div>
   </div>;
 
 const SkillsWrap = styled.div`
   margin-top: 5px;
-	margin-left: -2px;
-	margin-right: -2px;
-	.skill-type-wrap{
-    &, .tag-group{
+  margin-left: -2px;
+  margin-right: -2px;
+  .skill-type-wrap {
+    &,
+    .tag-group {
       display: inline;
     }
 
-    label{display: none;}
-	}
+    label {
+      display: none;
+    }
+  }
 `;
 
 const SkillsList = ({ skills }) =>
   <SkillsWrap>
     {Object.keys(skills).map((skillType, key) => {
-      console.log(skillType);
       return <TagGroup key={key} label={skillType} tags={skills[skillType]} />;
     })}
   </SkillsWrap>;
